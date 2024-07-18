e427fc4fe3f19a3e8a008999a7bee5bfd596e073
Ajaydodiya <ajaysinghdodiya29@gmail.com>
2024-07-18 15:27:24 +0530

    trackers added

--- Public/script.js
+++ Public/script.js
@@ -1,37 +1,54 @@
 const socket = io();
 
-if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition((position) => {
+socket.on('connect', () => {
+    console.log('Connected to server');
+});
 
-        const {latitude, longitude} = position.coords;
-        socket.emit("send-location",{ latitude, longitude});
+socket.on('connect_error', (error) => {
+    console.error('Connection error:', error);
+});
 
-    },
-    (error) => {
-        console.error(error);
-    },
-    {
-        enableHighAccuracy:true,
-        timeout:5000,
-        maximumAge:0,
-    }
-)}
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+        (position) => {
+            const { latitude, longitude } = position.coords;
+            console.log('Sending location:', { latitude, longitude });
+            socket.emit("send-location", { latitude, longitude });
+        },
+        (error) => {
+            console.error("Geolocation error:", error);
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0,
+        }
+    );
+} else {
+    console.error("Geolocation is not supported by this browser.");
+}
 
-const map = L.map("map").setView([0,0],16);
+const map = L.map("map").setView([0, 0], 3);
+
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:"Open Street Map By Ajay Dodiya"
-}).addTo(map)
+    attribution: "Open Street Map By Ajay Dodiya",
+}).addTo(map);
 
-const markers = {};
+const marker = {};
 
-socket.on("receive-location",(data)=>{
-    const {id,latitude, longitude} = data;
-    map.setView([latitude,longitude]);
-    if(markers[id]) {
-        markers[id].setLatLng ([latitude,longitude]);
+socket.on("receive-location", (data) => {
+    console.log('Received location data:', data);
+    const { id, latitude, longitude } = data;
+
+    if (latitude && longitude) {
+        map.setView([latitude, longitude]);
+
+        if (marker[id]) {
+            marker[id].setLatLng([latitude, longitude]);
+        } else {
+            marker[id] = L.marker([latitude, longitude]).addTo(map);
+        }
+    } else {
+        console.error("Invalid location data received:", data);
     }
-    else{
-        markers[id] = L.marker([latitude,longitude]).addTo(map);
-    }
-
 });
--- app.js
+++ app.js
@@ -7,22 +7,27 @@ const socketio = require("socket.io");
 const server = http.createServer(app);
 const io = socketio(server);
 const port = process.env.PORT || 3000;
-app.set("view engine", "ejs");
 
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-io.on("connection",function(socket){
-    socket.on("send-location",function(data){
-        io.emit("receive - location",{id:socket.id,...data})
+io.on("connection", function(socket) {
+    console.log("New user connected");
+
+    socket.on("send-location", function(data) {
+        console.log(`Location received from ${socket.id}:`, data);
+        io.emit("receive-location", { id: socket.id, ...data });
     });
-    console.log("New user connected")
-})
-app.get("/", function (req, res) {
-    res.render("index");
+
+    socket.on("disconnect", function() {
+        console.log("User disconnected");
+    });
 });
 
+app.get("/", function(req, res) {
+    res.render("index");
+});
 
 server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
-
