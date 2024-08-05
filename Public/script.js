a3d68a991f4acc382d4c9e351795cf9445c844d2
Ajaydodiya <ajaysinghdodiya29@gmail.com>
2024-08-05 16:58:27 +0530

    Revert "Initial commit"
    This reverts commit 8228d641229c4a9cbfe425bda9089342335ec5cb.

--- Public/script.js
+++ Public/script.js
@@ -28,7 +28,7 @@ if (navigator.geolocation) {
     console.error("Geolocation is not supported by this browser.");
 }
 
-const map = L.map("map").setView([0, 0], 10);
+const map = L.map("map").setView([0, 0], 3);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "Open Street Map By Ajay Dodiya",
